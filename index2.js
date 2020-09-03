f1dbf4c64640c17e6443583a22ae933f5fccedc8
Anya Efimova <gani4ka_n@ukr.net>
2020-09-04 00:37:54 +0300

    hw2-v1

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const path = require("path");
 const config = require("./config");
 const { send } = require("process");
-const usersRouter = require("./routers/contacts-router.js");
+const contactsRouter = require("./routers/contacts-router.js");
 
 const app = express();
 const port = 3000;
@@ -12,10 +12,7 @@ app.use(morgan("tiny"));
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.use("/", usersRouter);
-// app.get("/", (request, response) => {
-//   response.send("Hello from Express!");
-// });
+app.use("/", contactsRouter);
 
 app.use((err, req, res, next) => {
   if (err) {
--- routers/contacts-router.js
+++ routers/contacts-router.js
@@ -1,13 +1,16 @@
 const joi = require("joi");
 const express = require("express");
-const validate = require("../helpers/validator.js");
 const router = express.Router();
+
+const validate = require("../helpers/validator.js");
+
 const {
   contactsPath,
   listContacts,
   getContactById,
   removeContact,
   addContact,
+  updateContact,
 } = require("../contacts.js");
 
 router.get("/", async (req, res) => {
@@ -35,7 +38,9 @@ router.post("/", async (req, res) => {
   if (validation) {
     const { name, email, phone } = req.body;
 
-    const result = await addContact(name, email, phone);
+    const result = await addContact(name, email, phone).then(
+      (data) => data[data.length - 1]
+    );
 
     res.status(201).send(result);
   } else {
@@ -45,6 +50,40 @@ router.post("/", async (req, res) => {
       })
     );
   }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  const result = await removeContact(id);
+
+  if (result) {
+    res.send({ message: "contact deleted" });
+  } else res.status(404).send({ message: "Not found" });
+});
+
+router.patch("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  const body = req.body;
+
+  if (Object.keys(body).length === 0) {
+    res.status(400).send({ message: "missing fields" });
+    return;
+  }
+
+  const schema = joi.object({
+    name: joi.string().min(3),
+    email: joi.string(),
+    phone: joi.string().min(7),
+  });
+
+  const validation = validate(schema, body);
+
+  if (validation) {
+    const result = await updateContact(id, body);
+    res.send(JSON.stringify(result));
+  } else res.status(400).send({ message: "failed fields validation" });
 });
 
 module.exports = router;
